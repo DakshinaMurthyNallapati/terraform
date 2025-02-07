c3a530b4629d5dbaa288c8cd9d639baedd775cbc
Dakshina Murthy N <dakshinamurthynallapati@gmail.com>
2025-02-07 23:11:04 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,13 @@
+variable "instances" {
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+    default = "Z02764323F8JOQMF4PP9J"
+  
+}
+
+variable "domain_name" {
+    default = "dakshina.cloud"
+  
+}
